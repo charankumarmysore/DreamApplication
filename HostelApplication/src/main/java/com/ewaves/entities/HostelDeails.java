85ae512446fd3f7d4c47716a30c425cb8d44594a
Ewaves@Test <Ewaves@Test@DELL-PC>
2017-02-25 19:52:56 +0530

    New HostelDeails Model is add

---
+++ HostelApplication/src/main/java/com/ewaves/entities/HostelDeails.java
@@ -0,0 +1,20 @@
+package com.ewaves.entities;
+
+import javax.persistence.Entity;
+
+@Entity
+public class HostelDeails {
+
+	private Integer id;
+	private String hostelName;
+	private String primaryName;
+	private String primaryphoneNumber;
+	private String primaryemail;
+	private String primaryAddress;
+	private String secondaryName;
+	private String secondaryPhoneNumber;
+	private String secondaryEmail;
+	private String secondaryAddress;
+	 
+
+}
