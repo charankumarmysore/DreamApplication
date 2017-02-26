2813b443e6dc7eb267b86efe89909b78402b05e9
cherry <cherry@cherry-K53SD>
2017-02-26 14:51:03 +0530

    new Objects added

--- HostelApplication/src/main/java/com/ewaves/entities/HostelDeails.java
+++ HostelApplication/src/main/java/com/ewaves/entities/HostelDeails.java
@@ -1,19 +1,110 @@
 package com.ewaves.entities;
 
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
-public class HostelDeails {
+public class HostelDeails implements Serializable {
 
-	private Integer id;
+	private static final long serialVersionUID = 7749423133637138708L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer hostelId;
 	private String hostelName;
-	private String primaryName;
-	private String primaryphoneNumber;
-	private String primaryemail;
-	private String primaryAddress;
-	private String secondaryName;
-	private String secondaryPhoneNumber;
-	private String secondaryEmail;
-	private String secondaryAddress;
+	private String firstName;
+	private String lastName;
+	private String phoneNumber;
+	private String emailId;
+	private String address1;
+	private String address2;
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "hostelDeails", orphanRemoval = true)
+	private List<HostelFaculties> facultiesList;
+
+	public Integer getHostelId() {
+		return hostelId;
+	}
+
+	public void setHostelId(Integer hostelId) {
+		this.hostelId = hostelId;
+	}
+
+	public String getHostelName() {
+		return hostelName;
+	}
+
+	public void setHostelName(String hostelName) {
+		this.hostelName = hostelName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public String getAddress1() {
+		return address1;
+	}
+
+	public void setAddress1(String address1) {
+		this.address1 = address1;
+	}
+
+	public String getAddress2() {
+		return address2;
+	}
+
+	public void setAddress2(String address2) {
+		this.address2 = address2;
+	}
+
+	public List<HostelFaculties> getFacultiesList() {
+		return facultiesList;
+	}
+
+	public void setFacultiesList(List<HostelFaculties> facultiesList) {
+		this.facultiesList = facultiesList;
+	}
+
+	@Override
+	public String toString() {
+		return "HostelDeails [hostelId=" + hostelId + ", hostelName=" + hostelName + ", firstName=" + firstName
+				+ ", lastName=" + lastName + ", phoneNumber=" + phoneNumber + ", emailId=" + emailId + ", address1="
+				+ address1 + ", address2=" + address2 + ", facultiesList=" + facultiesList + "]";
+	}
 
 }
---
+++ HostelApplication/src/main/java/com/ewaves/entities/HostelFaculties.java
@@ -0,0 +1,100 @@
+package com.ewaves.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class HostelFaculties implements Serializable {
+
+	private static final long serialVersionUID = -2093306356754450050L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	private boolean tv;
+	private boolean wifi;
+	private boolean ac;
+	private boolean nonVegetarian;
+	private boolean washingMachine;
+	private boolean hotWater;
+	@ManyToOne(cascade = CascadeType.MERGE)
+	private HostelDeails hostelDeails;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public boolean isTv() {
+		return tv;
+	}
+
+	public void setTv(boolean tv) {
+		this.tv = tv;
+	}
+
+	public boolean isWifi() {
+		return wifi;
+	}
+
+	public void setWifi(boolean wifi) {
+		this.wifi = wifi;
+	}
+
+	public boolean isAc() {
+		return ac;
+	}
+
+	public void setAc(boolean ac) {
+		this.ac = ac;
+	}
+
+	public boolean isNonVegetarian() {
+		return nonVegetarian;
+	}
+
+	public void setNonVegetarian(boolean nonVegetarian) {
+		this.nonVegetarian = nonVegetarian;
+	}
+
+	public boolean isWashingMachine() {
+		return washingMachine;
+	}
+
+	public void setWashingMachine(boolean washingMachine) {
+		this.washingMachine = washingMachine;
+	}
+
+	public boolean isHotWater() {
+		return hotWater;
+	}
+
+	public void setHotWater(boolean hotWater) {
+		this.hotWater = hotWater;
+	}
+
+	public HostelDeails getHostelDeails() {
+		return hostelDeails;
+	}
+
+	public void setHostelDeails(HostelDeails hostelDeails) {
+		this.hostelDeails = hostelDeails;
+	}
+
+	@Override
+	public String toString() {
+		return "HostelFaculties [id=" + id + ", tv=" + tv + ", wifi=" + wifi + ", ac=" + ac + ", nonVegetarian="
+				+ nonVegetarian + ", washingMachine=" + washingMachine + ", hotWater=" + hotWater + ", hostelDeails="
+				+ hostelDeails + "]";
+	}
+
+}
